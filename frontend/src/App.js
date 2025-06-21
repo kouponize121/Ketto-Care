2407ce8c05322f79fd74ea7777d34decd5d1e452
kouponize121 <kouponize121@gmail.com>
2025-06-21 13:22:22 +0530

    Update App.js
    Update API URL to use localhost for local development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ import {
 import './App.css';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+const API = `http://localhost:8000/api`;
 
 // AuthContext
 const AuthContext = React.createContext();
